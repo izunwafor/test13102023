3a0474fc8b5931e9f42343beaeaffe0deff3500d
izunwafor <86169398+izunwafor@users.noreply.github.com>
2023-10-13 14:26:12 +0100

    Initial commit

---
+++ src/main/java/com/izu13102023/test/TestApplication.java
@@ -0,0 +1,13 @@
+package com.izu13102023.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
